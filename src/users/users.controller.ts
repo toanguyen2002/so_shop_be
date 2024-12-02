c21b2b728f5b5d572bd0d255cb21de121d995816
toannq13 <toanguyen200220@gmail.com>
2024-12-02 16:59:48 +0700

    feat: change pass

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Public, Roles, UserGuard } from './guard/user.guard';
 import { Role } from './enum/role.enum';
@@ -6,118 +14,120 @@ import { UserDTO } from './dto/user.dto';
 import { MailerService } from '@nestjs-modules/mailer';
 import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
-
 @Controller('users')
 export class UsersController {
-    constructor(
-        private readonly service: UsersService,
-        private readonly mailSrevice: MailerService
-    ) { }
+  constructor(
+    private readonly service: UsersService,
+    private readonly mailSrevice: MailerService,
+  ) {}
 
-    @Public()
-    @Post('/register')
-    async register(@Body() userDTO: UserDTO) {
-        return await this.service.signUp(userDTO.userName, userDTO.password)
+  @Public()
+  @Post('/register')
+  async register(@Body() userDTO: UserDTO) {
+    return await this.service.signUp(userDTO.userName, userDTO.password);
+  }
+
+  @Public()
+  @Post('/admin')
+  async init(@Body() userDTO: UserDTO) {
+    return await this.service.admin();
+  }
+
+  @Public()
+  @Post('/login')
+  async login(@Body() userDTO: UserDTO) {
+    const rs = await this.service.signIn(userDTO.userName, userDTO.password);
+    return rs;
+  }
+
+  @Public()
+  @Post('/reset')
+  async reset(@Body() userDTO: UserDTO) {
+    const rs = await this.service.resetPassWord(userDTO);
+    return rs;
+  }
+
+  @Roles(Role.BUYER)
+  @Get('profile/:id')
+  async getByUserName(@Param('id') id: string) {
+    return await this.service.getprofile(id);
+  }
+
+  // @Roles(Role.BUYER)
+  // @Get('admin')
+  // async admin(@Request() req) {
+  //     return req.user
+  // }
+
+  @Roles(Role.BUYER)
+  @Post('/update')
+  async updateByUser(@Body() userDTO: UserDTO) {
+    return await this.service.findByIdAndUpdateUser(userDTO);
+  }
+  @Roles(Role.ADMIN)
+  // @Public()
+  @Post('/admin/update')
+  async updateSeller(@Body() data: { id: string }) {
+    return await this.service.acceptSeller(data.id);
+  }
+
+  // @Public()
+  @Roles(Role.BUYER)
+  @Post('/buyer/register')
+  async registerSeller(@Body() data: { id: string }) {
+    return await this.service.registerSeller(data.id);
+  }
+  @Roles(Role.ADMIN)
+  // @Public()
+  @Get('/admin/getSeller')
+  async getSeller() {
+    return await this.service.findAllRegisterUser();
+  }
+  @Public()
+  @Post('sendOTP')
+  async getOTP(@Body() user: UserDTO) {
+    const exitsUser = await this.service.checkMail(user.userName);
+    console.log(exitsUser);
+
+    if (exitsUser > 0) {
+      return {
+        code: 500,
+        status: 'Email is exist in Systerm',
+      };
     }
-
-    @Public()
-    @Post('/admin')
-    async init(@Body() userDTO: UserDTO) {
-        return await this.service.admin()
+    let randomOTPnumber = '';
+    for (let index = 0; index < 6; index++) {
+      randomOTPnumber += Math.floor(Math.random() * 10).toString();
     }
-
-    @Public()
-    @Post("/login")
-    async login(@Body() userDTO: UserDTO) {
-        const rs = await this.service.signIn(userDTO.userName, userDTO.password)
-        return rs
-    }
-
-
-    @Public()
-    @Post("/reset")
-    async reset(@Body() userDTO: UserDTO) {
-        const rs = await this.service.resetPassWord(userDTO)
-        return rs
-    }
-
-
-    @Roles(Role.BUYER)
-    @Get('profile/:id')
-    async getByUserName(@Param("id") id: string) {
-        return await this.service.getprofile(id)
-    }
-
-    // @Roles(Role.BUYER)
-    // @Get('admin')
-    // async admin(@Request() req) {
-    //     return req.user
-    // }
-
-    @Roles(Role.BUYER)
-    @Post("/update")
-    async updateByUser(@Body() userDTO: UserDTO) {
-        return await this.service.findByIdAndUpdateUser(userDTO)
-
-    }
-    @Roles(Role.ADMIN)
-    // @Public()
-    @Post("/admin/update")
-    async updateSeller(@Body() data: { id: string }) {
-        return await this.service.acceptSeller(data.id)
-    }
-
-    // @Public()
-    @Roles(Role.BUYER)
-    @Post("/buyer/register")
-    async registerSeller(@Body() data: { id: string }) {
-        return await this.service.registerSeller(data.id);
-    }
-    @Roles(Role.ADMIN)
-    // @Public()
-    @Get("/admin/getSeller")
-    async getSeller() {
-        return await this.service.findAllRegisterUser()
-    }
-    @Public()
-    @Post("sendOTP")
-    async getOTP(@Body() user: UserDTO) {
-        const exitsUser = await this.service.checkMail(user.userName)
-        console.log(exitsUser);
-
-        if (exitsUser > 0) {
-            return {
-                code: 500,
-                status: "Email is exist in Systerm"
-            }
-        }
-        let randomOTPnumber = ""
-        for (let index = 0; index < 6; index++) {
-            randomOTPnumber += Math.floor(Math.random() * 10).toString()
-        }
-        try {
-            await this.mailSrevice.sendMail({
-                to: user.userName, // list of receivers
-                from: 'noreply@osshop.com', // sender address
-                subject: 'OTP from os shop ✔', // Subject line
-                text: 'welcome',
-                html: `
+    try {
+      await this.mailSrevice.sendMail({
+        to: user.userName, // list of receivers
+        from: 'noreply@osshop.com', // sender address
+        subject: 'OTP from os shop ✔', // Subject line
+        text: 'welcome',
+        html: `
                     <div class="content">
                     <p>Dear </p>
 
             <p>Your OTP IS ${randomOTPnumber}</p>
-        </div>`
-            })
-            return randomOTPnumber
-        } catch (error) {
-            throw new ExceptionsHandler
-        }
+        </div>`,
+      });
+      return randomOTPnumber;
+    } catch (error) {
+      throw new ExceptionsHandler();
     }
-    @Public()
-    @Get("findtop10")
-    async findTop10() {
-        return await this.service.top10brand()
-    }
+  }
+  @Public()
+  @Get('findtop10')
+  async findTop10() {
+    return await this.service.top10brand();
+  }
 
-
+  @Post('changePass')
+  async changePass(@Body() data: { userName: string; passW: string }) {
+    return await this.service.changePassWord({
+      newPass: data.passW,
+      userName: data.userName,
+    });
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,150 +11,158 @@ import { MailerService } from '@nestjs-modules/mailer';
 export type User = any;
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel(Users.name) private readonly model: Model<UsersDocument>,
-        private readonly jwtService: JwtService,
-        private readonly mailer: MailerService
-    ) { }
-    async signIn(
-        userName: string,
-        pass: string,
-    ): Promise<User> {
-        const existUser = await this.model.aggregate([{
-            $match: { userName: userName }
-        }])
+  constructor(
+    @InjectModel(Users.name) private readonly model: Model<UsersDocument>,
+    private readonly jwtService: JwtService,
+    private readonly mailer: MailerService,
+  ) {}
+  async signIn(userName: string, pass: string): Promise<User> {
+    const existUser = await this.model.aggregate([
+      {
+        $match: { userName: userName },
+      },
+    ]);
 
-        if (existUser.length > 0) {
-            if (await bcrypt.compare(pass, existUser[0].password)) {
-                const { password, ...payload } = existUser[0]
-                return {
-                    ...payload,
-                    access_token: await this.jwtService.signAsync(payload),
-                    code: 200
-                }
-            } else {
-                return {
-                    status: "Mật khẩu không chính xác",
-                    code: 201
-                }
-            }
-        } else {
-            return {
-                status: "Tài khoản không tồn tại",
-                code: 201
-            }
-        }
+    if (existUser.length > 0) {
+      if (await bcrypt.compare(pass, existUser[0].password)) {
+        const { password, ...payload } = existUser[0];
+        return {
+          ...payload,
+          access_token: await this.jwtService.signAsync(payload),
+          code: 200,
+        };
+      } else {
+        return {
+          status: 'Mật khẩu không chính xác',
+          code: 201,
+        };
+      }
+    } else {
+      return {
+        status: 'Tài khoản không tồn tại',
+        code: 201,
+      };
     }
-    async admin(): Promise<User> {
-        const hash = await bcrypt.hash("123456", 10);
-        return await new this.model({
-            userName: "admin",
-            password: hash,
-            isRegister: true,
-            role: ["BUYER", "ADMIN", "SELLER"]
-        }).save();
+  }
+  async admin(): Promise<User> {
+    const hash = await bcrypt.hash('123456', 10);
+    return await new this.model({
+      userName: 'admin',
+      password: hash,
+      isRegister: true,
+      role: ['BUYER', 'ADMIN', 'SELLER'],
+    }).save();
+  }
+  async signUp(userName: string, password: string): Promise<User> {
+    const existUser = await this.model.aggregate([
+      {
+        $match: { userName: userName },
+      },
+    ]);
+    if (existUser.length > 0) {
+      return {
+        status: 'user đã tồn tại',
+        code: 201,
+      };
     }
-    async signUp(
-        userName: string,
-        password: string)
-        : Promise<User> {
-        const existUser = await this.model.aggregate([{
-            $match: { userName: userName }
-        }])
-        if (existUser.length > 0) {
-            return {
-                status: "user đã tồn tại",
-                code: 201
-            }
-        }
-        const hash = await bcrypt.hash(password, 10);
-        return await new this.model({
-            userName: userName,
-            password: hash,
-            isRegister: false,
-            role: ["BUYER"]
-        }).save();
-    }
+    const hash = await bcrypt.hash(password, 10);
+    return await new this.model({
+      userName: userName,
+      password: hash,
+      isRegister: false,
+      role: ['BUYER'],
+    }).save();
+  }
 
-    async getprofile(id: string): Promise<User> {
-        const existUser = await this.model.aggregate([{
-            $match: { _id: new mongoose.Types.ObjectId(id) }
-        }])
-        const { password, ...payload } = existUser[0]
-        return payload
+  async getprofile(id: string): Promise<User> {
+    const existUser = await this.model.aggregate([
+      {
+        $match: { _id: new mongoose.Types.ObjectId(id) },
+      },
+    ]);
+    const { password, ...payload } = existUser[0];
+    return payload;
+  }
+  async checkMail(userName: string): Promise<User> {
+    const existUser = await this.model.aggregate([
+      {
+        $match: { userName: userName },
+      },
+    ]);
+    return existUser.length;
+  }
+  async findByIdAndUpdateUser(userDTO: UserDTO): Promise<User> {
+    if (userDTO.password !== undefined) {
+      const hash = await bcrypt.hash(userDTO.password, 10);
+      userDTO.password = hash;
+      const { password, ...payload } = userDTO;
+      console.log(userDTO);
+      try {
+        await this.model.findByIdAndUpdate(userDTO.id, userDTO);
+        return payload;
+      } catch (error) {
+        throw new error();
+      }
+    } else {
+      try {
+        await this.model.findByIdAndUpdate(userDTO.id, userDTO);
+        return userDTO;
+      } catch (error) {
+        throw new error();
+      }
     }
-    async checkMail(userName: string): Promise<User> {
-        const existUser = await this.model.aggregate([{
-            $match: { userName: userName }
-        }])
-        return existUser.length
-    }
-    async findByIdAndUpdateUser(userDTO: UserDTO): Promise<User> {
-        if (userDTO.password !== undefined) {
-            const hash = await bcrypt.hash(userDTO.password, 10);
-            userDTO.password = hash
-            const { password, ...payload } = userDTO
-            console.log(userDTO);
-            try {
-                await this.model.findByIdAndUpdate(userDTO.id, userDTO)
-                return payload
-            } catch (error) {
-                throw new error
-            }
-        } else {
-            try {
-                await this.model.findByIdAndUpdate(userDTO.id, userDTO)
-                return userDTO
-            } catch (error) {
-                throw new error
-            }
-        }
-    }
+  }
 
-    async registerSeller(id: string): Promise<User> {
-        const existUser = await this.model.aggregate([{
-            $match: { _id: new mongoose.Types.ObjectId(id) }
-        }])
-        existUser[0].isRegister = true
+  async registerSeller(id: string): Promise<User> {
+    const existUser = await this.model.aggregate([
+      {
+        $match: { _id: new mongoose.Types.ObjectId(id) },
+      },
+    ]);
+    existUser[0].isRegister = true;
 
-        return await this.model.findByIdAndUpdate(id, existUser[0], { new: true })
+    return await this.model.findByIdAndUpdate(id, existUser[0], { new: true });
+  }
+
+  async findAllRegisterUser(): Promise<User> {
+    return await this.model.aggregate([
+      {
+        $match: { isRegister: true, role: { $nin: ['SELLER', 'ADMIN'] } },
+      },
+    ]);
+  }
+
+  async acceptSeller(id: string): Promise<User> {
+    const existUser = await this.model.aggregate([
+      {
+        $match: { _id: new mongoose.Types.ObjectId(id) },
+      },
+    ]);
+    existUser[0].role.push('SELLER');
+    return await this.model.findByIdAndUpdate(id, existUser[0], { new: true });
+  }
+
+  async resetPassWord(userDTO: UserDTO): Promise<User> {
+    let newPass = '';
+    for (let index = 0; index < 5; index++) {
+      newPass += Math.floor(Math.random() * 10).toString();
     }
-
-    async findAllRegisterUser(): Promise<User> {
-        return await this.model.aggregate([{
-            $match: { isRegister: true, role: { $nin: ["SELLER", "ADMIN"] } }
-        }])
-    }
-
-    async acceptSeller(id: string): Promise<User> {
-        const existUser = await this.model.aggregate([{
-            $match: { _id: new mongoose.Types.ObjectId(id) }
-        }])
-        existUser[0].role.push('SELLER')
-        return await this.model.findByIdAndUpdate(id, existUser[0], { new: true })
-    }
-
-    async resetPassWord(userDTO: UserDTO): Promise<User> {
-        let newPass = ""
-        for (let index = 0; index < 5; index++) {
-            newPass += Math.floor(Math.random() * 10).toString()
-        }
-        const hash = await bcrypt.hash(newPass, 10);
-        const existUser = await this.model.aggregate([{
-            $match: { userName: userDTO.userName }
-        }])
-        if (existUser.length > 0) {
-            console.log(await bcrypt.compare(newPass, hash));
-            existUser[0].password = hash
-            await this.model.findByIdAndUpdate(existUser[0]._id, existUser[0])
-            await this.mailer
-                .sendMail({
-                    to: existUser[0].userName, // list of receivers
-                    from: 'noreply@osshop.com', // sender address
-                    subject: 'Reset Password', // Subject line
-                    text: 'Dear', // plaintext body
-                    // html: this.templateCancel({ name: "OS Shop", tradeId: tradeDTO.tradeId }), // HTML body content
-                    html: `
+    const hash = await bcrypt.hash(newPass, 10);
+    const existUser = await this.model.aggregate([
+      {
+        $match: { userName: userDTO.userName },
+      },
+    ]);
+    if (existUser.length > 0) {
+      existUser[0].password = hash;
+      await this.model.findByIdAndUpdate(existUser[0]._id, existUser[0]);
+      await this.mailer.sendMail({
+        to: existUser[0].userName, // list of receivers
+        from: 'noreply@osshop.com', // sender address
+        subject: 'Reset Password', // Subject line
+        text: 'Dear', // plaintext body
+        // html: this.templateCancel({ name: "OS Shop", tradeId: tradeDTO.tradeId }), // HTML body content
+        html: `
                     <div class="content">
                     <p>Dear You</p>
 
@@ -163,46 +171,78 @@ export class UsersService {
                 <p>${newPass} is your new password</p>
 
                 <p>Sincerely</p>
-        </div>`
-                })
-            return {
-                status: "yêu cầu thành công",
-                code: 200
+        </div>`,
+      });
+      return {
+        status: 'yêu cầu thành công',
+        code: 200,
+      };
+    } else {
+      throw new Exception('can not exist!');
+    }
+  }
+  async top10brand() {
+    return await this.model.aggregate([
+      {
+        $lookup: {
+          from: 'products',
+          localField: '_id',
+          foreignField: 'seller',
+          as: 'products',
+        },
+      },
+      {
+        $unwind: '$products',
+      },
+      {
+        $group: {
+          _id: '$_id',
+          name: { $first: '$name' },
+          avata: { $first: '$avata' },
+          address: { $first: '$address' },
+          selled: {
+            $sum: '$products.selled',
+          },
+        },
+      },
+      {
+        $sort: { selled: 1 },
+      },
+      {
+        $limit: 10,
+      },
+    ]);
+  }
+  async changePassWord(data: { newPass: string; userName: string }) {
+    const hash = await bcrypt.hash(data.newPass, 10);
+    const existUser = await this.model.aggregate([
+      {
+        $match: { userName: data.userName },
+      },
+    ]);
+    if (existUser.length) {
+      existUser[0].password = hash;
+      await this.mailer.sendMail({
+        to: existUser[0].userName, // list of receivers
+        from: 'noreply@osshop.com', // sender address
+        subject: 'Reset Password', // Subject line
+        text: 'Dear', // plaintext body
+        // html: this.templateCancel({ name: "OS Shop", tradeId: tradeDTO.tradeId }), // HTML body content
+        html: `
+                    <div class="content">
+                    <p>Dear You</p>
 
-            }
-        } else {
-            throw new Exception('can not exist!')
-        }
+                <p>confirm that your order reset your password</p>
+
+                <p>change Pass success</p>
+
+                <p>Sincerely</p>
+        </div>`,
+      });
+      return {
+        status: 'yêu cầu thành công',
+        code: 200,
+      };
     }
-    async top10brand() {
-        return await this.model.aggregate([
-            {
-                $lookup: {
-                    from: "products",
-                    localField: "_id",
-                    foreignField: "seller",
-                    as: "products"
-                }
-            },
-            {
-                $unwind: "$products"
-            },
-            {
-                $group: {
-                    _id: "$_id",
-                    name: { $first: "$name" },
-                    avata: { $first: "$avata" },
-                    address: { $first: "$address" },
-                    selled: {
-                        $sum: "$products.selled"
-                    }
-                }
-            }, {
-                $sort: { selled: 1 }
-            },
-            {
-                $limit: 10
-            }
-        ])
-    }
+  }
 }
